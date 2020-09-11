37a14f4f6fa7fa754b06b2ba3dad4a06c0ec6df8
Christian Nell <46611006+cnell7@users.noreply.github.com>
2020-09-11 17:01:57 -0400

    Submission 1

--- SMTP2.py
+++ SMTP2.py
@@ -62,54 +62,66 @@ def toParse(string):
     #   Checkes for "To: " and a mailbox in a forward file
     if(string[0:3] != "To:"):
         return False
-    string = string[3:]
+    string = string[4:]
     rcptTo.append(string)
     return string
 
 
 def dataParse(string):
     #   Checks for Data in the forward file
-    print(string)
+    sys.stdout.write(string)
     data.append(string)
     return True
 
 
-def responseCodeChecker():
+def responseCodeChecker(state):
     #   Checks the server's response
+    #    <resp-number>
     temp = sys.stdin.readline()
-    print(temp.rstrip(), file=sys.stderr)
+    copy = temp[3:]
+    sys.stderr.write(temp)
     code = temp[0:3]
     if(not(code == "250" or code == "354" or code == "500" or code == "501")):
         return False
+    if(state == 0 or state == 1):
+        if(code != "250"):
+            return False
+    elif(state == 2):
+        if(code != "354"):
+            return False
+    #   <whitespace>
+    if(copy[0] == whitespace(copy)[0]):
+        return False
+    #   <arbitrary-text> <CRLF>
     return True
 
 
 def call_command(string, state):
-    #   Checks to make sure file has correct From:, To: and Data parameters then prints
+    #   Checks to make sure file has correct From:, To: and Data parameters then logs to standard out
     isTrue = False
     if(state == 0):  # State 0 = From:
         string = fromParse(string)
-        print("MAIL FROM: " + string)
-        if(not(responseCodeChecker())):
+        sys.stdout.write("MAIL FROM: " + string)
+        if(not(responseCodeChecker(0))):
             return -1
         return 1
     elif(state == 1):  # State 1 = To:
         isTrue = toParse(string)
         if(isTrue == False):  # Move to state 2
-            print("DATA")
-            if(not(responseCodeChecker())):
+            sys.stdout.write("DATA\n")
+            if(not(responseCodeChecker(2))):
                 return -1
             return call_command(string, 2)
         string = toParse(string)
-        print("RCPT TO: " + string)
-        if(not(responseCodeChecker())):
+        sys.stdout.write("RCPT TO: " + string)
+        if(not(responseCodeChecker(1))):
             return -1
         return 1
     elif(state == 2):  # State 2 = Data
         isTrue = fromParse(string)
         if(isTrue):  # Move back to state 0
-            print(".")
-            if(not(responseCodeChecker())):
+            sys.stdout.write(".\n")
+            if(not(responseCodeChecker(0))):
                 return -1
             return call_command(string, 0)
         dataParse(string)
@@ -125,14 +137,14 @@ def main():
             if(state == 0):
                 rcptTo.clear()
                 data.clear()
-            state = call_command(line.rstrip(), state)
+            state = call_command(line, state)
             if(state == -1):
                 break
-    if(not(state == -1)):
-        print(".")
-        if(not(responseCodeChecker())):
-            return -1
-    print("QUIT")
+    if(not(state == -1)):  # End of file
+        sys.stdout.write(".\n")
+        responseCodeChecker(0)
+
+    sys.stdout.write("QUIT\n")
     return False
 
 
